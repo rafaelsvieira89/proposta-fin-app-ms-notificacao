dc3e7609689bdcf9ac075f3828222f76546de51e
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-22 11:00:48 -0300

    Add domain models and RabbitMQ configuration
    Introduce `Usuario` and `Proposta` domain classes to represent core entities. Update `application.yaml` and add metadata configuration to define the pending proposals queue for RabbitMQ integration. These changes set the foundation for notification handling.

---
+++ src/main/java/com/vieirarafael/propostafinappmsnotificacao/domain/Proposta.java
@@ -0,0 +1,17 @@
+package com.vieirarafael.propostafinappmsnotificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Proposta {
+    private Long id;
+
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private boolean integrada;
+    private String observacao;
+    private Usuario usuario;
+}
---
+++ src/main/java/com/vieirarafael/propostafinappmsnotificacao/domain/Usuario.java
@@ -0,0 +1,17 @@
+package com.vieirarafael.propostafinappmsnotificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+    private Long id;
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+
+}
