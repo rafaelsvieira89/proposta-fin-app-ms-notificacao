07b7b9272368aa46de87fdfef503a6914c064318
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-22 10:49:00 -0300

    Initialize Spring Boot microservice project with Gradle
    Added a new Spring Boot project configured with Gradle as the build tool. Includes default configurations, dependencies for AMQP, and testing with JUnit. Git and IDE-specific ignore files were also added.

---
+++ src/test/java/com/vieirarafael/propostafinappmsnotificacao/PropostaFinAppMsNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieirarafael.propostafinappmsnotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaFinAppMsNotificacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
